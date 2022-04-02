46923acb97ecbb187dadc40c344e3ceb70888b25
xiaodandandandan <XiaoyanZhang0606@outlook.com>
2022-04-02 21:26:09 +0800

    weekend

--- src/pages/home/components/Weekend.vue
+++ src/pages/home/components/Weekend.vue
@@ -1,13 +1,14 @@
 <template>
  <div>
-    <div class="title">热销推荐</div>
+    <div class="title">周末去哪儿</div>
     <ul>
-        <li class="item" v-for="item of recommendList" :key="item.id">
-            <img class="item-img" :src="item.imgUrl">
+        <li class="item border-bottom" v-for="item of recommendList" :key="item.id">
+            <div class="item-img-wrapper">
+                <img class="item-img" :src="item.imgUrl">
+            </div>
             <div class="item-info">
                 <p class="item-title">{{item.title}}</p>
                 <p class="item-desc">{{item.desc}}</p>
-                <button class="item-button">查看详情</button>
             </div>
         </li>
     </ul>
@@ -16,25 +17,25 @@
 
 <script>
 export default {
-    name:'HomeRecommend',
+    name:'HomeWeekend',
     data() {
         return {
             recommendList:[
                 {
                     id:'001',
-                    imgUrl:'https://imgs.qunarzz.com/p/p67/1512/a2/0ebfcd965b9391f7.jpg_256x160_04d5813d.jpg',
+                    imgUrl:'https://img1.qunarzz.com/sight/p0/2005/39/3979f1867defec4ea3.water.jpg_256x160_6ae804bb.jpg',
                     title:'大连圣亚海洋世界',
                     desc:'浪漫大连首站，浪漫的海浪漫.大连首站，浪漫的海.浪漫大连首站，浪漫的海'
                 },
                 {
                     id:'002',
-                    imgUrl:'https://imgs.qunarzz.com/p/p67/1512/a2/0ebfcd965b9391f7.jpg_256x160_04d5813d.jpg',
+                    imgUrl:'https://img1.qunarzz.com/sight/p0/2005/39/3979f1867defec4ea3.water.jpg_256x160_6ae804bb.jpg',
                     title:'大连圣亚海洋世界',
                     desc:'浪漫大连首站，浪漫的海浪漫.大连首站，浪漫的海.浪漫大连首站，浪漫的海'
                 },
                 {
                     id:'003',
-                    imgUrl:'https://imgs.qunarzz.com/p/p67/1512/a2/0ebfcd965b9391f7.jpg_256x160_04d5813d.jpg',
+                    imgUrl:'https://img1.qunarzz.com/sight/p0/2005/39/3979f1867defec4ea3.water.jpg_256x160_6ae804bb.jpg',
                     title:'大连圣亚海洋世界',
                     desc:'浪漫大连首站，浪漫的海浪漫.大连首站，浪漫的海.浪漫大连首站，浪漫的海'
                 },
@@ -63,32 +64,28 @@ export default {
         line-height: .8rem
         background-color: #eee
         text-indent:.2rem
-    .item
-        overflow hidden
-        display: flex
-        height:1.9rem
-        padding:.1rem
+    .item-img-wrapper
+        overflow: hidden
+        height: 0
+        padding-bottom: 37.09%
         .item-img
-            width: 1.7rem
-            height: 1.7rem
-            padding: .1rem
-        .item-info
-            flex:1
-            padding: .1rem
-            min-width 0
-            .item-title
-                height: .54rem
-                font-size: 0.32rem
-            .item-desc 
-                line-height: .4rem
-                color: #999
-                ellipsis()
-            .item-button
-                line-height:.44rem
-                margin-top: .16rem
-                background-color: #ff9300
-                padding:0 .1rem
-                border-radius: .06rem 
-                color:#fff
+            width: 100%
+    .item-info
+        padding: .1rem
+        min-width 0
+        .item-title
+            height: .54rem
+            font-size: 0.32rem
+        .item-desc 
+            line-height: .4rem
+            color: #999
+            ellipsis()
+        .item-button
+            line-height:.44rem
+            margin-top: .16rem
+            background-color: #ff9300
+            padding:0 .1rem
+            border-radius: .06rem 
+            color:#fff
 
 </style>
