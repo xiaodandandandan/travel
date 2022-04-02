3e3d15cf25e8e1f76a61e6ee2f1754f6a934cd0e
xiaodandandandan <XiaoyanZhang0606@outlook.com>
2022-04-02 20:53:16 +0800

    add recommend

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -3,6 +3,7 @@
     <HomeHeader/>
     <HomeSwiper/>
     <HomeIcons/>
+    <HomeRecommend/>
   </div>
 </template>
 
@@ -10,9 +11,10 @@
 import HomeHeader from './components/Header'
 import HomeSwiper from './components/Swiper'
 import HomeIcons from './components/Icons.vue'
+import HomeRecommend from './components/Recommend.vue'
 export default {
   name:'Home',
-  components:{HomeHeader,HomeSwiper,HomeIcons}
+  components:{HomeHeader,HomeSwiper,HomeIcons,HomeRecommend}
 }
 </script>
 
