0b1fad9555b072c04f6efaae915b0782b49cf168
xiaodandandandan <XiaoyanZhang0606@outlook.com>
2022-04-06 00:23:47 +0800

    细节优化 keep-alive不重复发送ajax请求 首页请求加上city参数

--- src/main.js
+++ src/main.js
@@ -4,24 +4,15 @@ import App from './App.vue'
 import VueRouter from 'vue-router'
 import router from './router/index'
 import store from './store'
-//解决移动端click事件300ms延迟执行
-import fastClick from 'fastclick'
 //swiper
 import VueAwesomeSwiper from 'vue-awesome-swiper'
-
-
 // import style
 // import 'swiper/css/swiper.css'
 import 'swiper/css/swiper.css'
-
 import './assets/style/reset.css'
 import './assets/style/border.css'
 import './assets/style/iconfont.css'
 
-
-
-
-fastClick.attach(document.body)
 Vue.config.productionTip = false
 Vue.use(VueRouter)
 Vue.use(VueAwesomeSwiper)
