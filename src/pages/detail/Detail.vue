30efaf96e16a32fed9d0d5bfaa1d4d16cded2a29
xiaodandandandan <XiaoyanZhang0606@outlook.com>
2022-04-06 17:07:15 +0800

    detail list

--- src/pages/detail/Detail.vue
+++ src/pages/detail/Detail.vue
@@ -2,18 +2,38 @@
  <div>
     <DetailBanner/>
     <DetailHeader/>
-    <div class="content">
-
-    </div>
+    <DetailList :list="list"/>
  </div>
 </template>
 
 <script>
 import DetailBanner from './components/Banner'
 import DetailHeader from './components/Header.vue'
+import DetailList from './components/List.vue'
 export default {
     name:'Detail',
-    components:{DetailBanner,DetailHeader}
+    components:{DetailBanner,DetailHeader,DetailList},
+    data() {
+        return {
+            list:[{
+                title:'成人票',
+                children:[{
+                    title:'成人三馆联票',
+                    children:[{
+                        title:'成人三馆联票--feizhu',
+                    }]
+                },{
+                    title:'成人五馆联票'
+                }]
+            },{
+                title:'学生票'
+            },{
+                title:'儿童票'
+            },{
+                title:'特惠票'
+            }]
+        }
+    },
 }
 </script>
 
